004afd19c8ab54d9fa7f0df2e37e568678a1b942
Xilosof <pro100temi444@gmail.com>
2020-07-19 22:33:32 +0300

    fsd

---
+++ PerfRegister.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleProfiler
+{
+    public static class PerfRegister
+    {
+        public static List<RegisterRecord> Records { get; set; } = new List<RegisterRecord>();
+
+        public static RegisterRecord? LastRecord
+        {
+            get
+            {
+                if (Records.Count > 0)
+                    return Records[Records.Count - 1];
+                else
+                    return null;
+            }
+
+        }
+    }
+
+    public struct RegisterRecord
+    {
+        public DateTime Start;
+        public DateTime Finish;
+        public TimeSpan Elapsed;
+
+        public RegisterRecord(DateTime Start,DateTime Finish,TimeSpan Elapsed)
+        {
+            this.Start = Start;
+            this.Finish = Finish;
+            this.Elapsed = Elapsed;
+        }
+    }
+}
