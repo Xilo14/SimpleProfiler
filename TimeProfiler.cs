9b6e2e6c94898292c47aa84b2aeee597e9bfdf89
Xilosof <pro100temi444@gmail.com>
2020-07-19 22:43:13 +0300

    Create simple time profiler. Use "using".

---
+++ TimeProfiler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SimpleProfiler
+{
+    public class TimeProfiler : IDisposable
+    {
+        private DateTime _start;
+        private DateTime _finish;
+        private Stopwatch _stopwatch;
+        public TimeProfiler()
+        {
+            _start = DateTime.Now;
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            _finish = DateTime.Now;
+
+            RegisterRecord record = new RegisterRecord(
+                this._start,
+                this._finish,
+                this._stopwatch.Elapsed);
+
+            PerfRegister.Records.Add(record);
+        }
+    }
+}
